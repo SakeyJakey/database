e1bfbafd2ef3a4314ce2cf2b267738f4d8656e5d
sakey <sakey@tuta.io>
2026-08-31 20:10:32 +0100

    add insert logic

--- db.c
+++ db.c
@@ -15,7 +15,8 @@ typedef struct {
   char *name;
 
   uint32_t rows;
-  void *data;
+  uint32_t cols;
+  char *data;
 } Table;
 
 Table **tables = NULL;
@@ -29,6 +30,7 @@ tableNew(Table *table, char *name, char *text[], int cols) {
   *table = (Table) {
     .name = strdup(name),
     .rows = 0,
+    .cols = cols / 2,
     .schema = malloc(sizeof *table->schema * cols / 2),
     .data = NULL
   };
@@ -80,11 +82,84 @@ commandNew(char *text)
   return NULL;
 }
 
-int main(void) {
-  commandNew(strdup("ages age n name s"));
+char *
+tableInsert(Table *t, char **items, size_t items_count)
+{
+  if (items_count != t->cols)
+    return "ins: Incorrect number of columns";
 
-  printf("%s", tables[0]->name);
-  printf("%s", tables[0]->schema[0].name);
-  printf("%s", tables[0]->schema[1].name);
+  size_t row_size = 0;
+  for (uint32_t i = 0; i < t->cols; i++) {
+    switch (t->schema[i].type) {
+    case TYPE_BOOL:
+      row_size += sizeof(int);
+      break;
+    case TYPE_BYTES: /* fallthrough */
+    case TYPE_STR:
+      row_size += sizeof(char *);
+      break;
+    case TYPE_NUM:
+      row_size += sizeof(double);
+      break;
+    }
+  }
+
+  t->data = realloc(t->data, row_size * (t->rows + 1));
+  char *insert = &t->data[t->rows * row_size];
+
+  for (uint32_t i = 0; i < t->cols; i++) {
+    switch (t->schema[i].type) {
+    case TYPE_BOOL:
+      if (items[i][0] == 't')
+        *(int *)insert = 0;
+      insert += sizeof(int);
+      break;
+    case TYPE_BYTES: /* fallthrough */
+    case TYPE_STR:
+      *(char **)insert = strdup(items[i]);
+      insert += sizeof(int);
+      break;
+    case TYPE_NUM:
+      *(double *)insert = atof(items[i]);
+      insert += sizeof(double);
+      break;
+    }
+  }
+
+  t->rows++;
+  return "ins: Success";
+}
+
+char *
+commandInsert(char *row)
+{
+  char **tokens = NULL, *token, *ctx;
+  size_t i, j;
+
+  for (i = 0, token = strtok_r(row, " ", &ctx);
+       token;
+       i++, token = strtok_r(NULL, " ", &ctx)) {
+    tokens = realloc(tokens, sizeof *tokens * (i + 1));
+    tokens[i] = strdup(token);
+  }
+
+  Table *t = NULL;
+  for (j = 0; j < tables_count; j++)
+    if (strcmp(tables[j]->name, tokens[0]) == 0)
+      t = tables[j];
+
+  if (!t) {
+    char *err = malloc(sizeof("ins: No table named ") + strlen(tokens[0])); /* sizeof includes null at end */
+    strcpy(err, "ins: No table named ");
+    strcat(err, tokens[0]);
+    return err;
+  }
+
+  return tableInsert(t, &tokens[1], i - 1);
+}
+
+int main(void) {
+  printf("%s", commandNew(strdup("ages age n name s")));
+  printf("%s", commandInsert(strdup("ages 10 joe")));
   return 0;
 }
